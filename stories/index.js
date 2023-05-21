68b67e59aad1fa1656593441fb7c3aa7a9145180
eliachow <eliacchow@gmail.com>
2023-05-21 13:04:07 -0700

    Create form component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,14 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList"
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 // ---  Button Stories --------------------------------------------------
@@ -145,3 +153,57 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+
+// ---  Appointment Stories --------------------------------------------------
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with time", () => (
+    <Appointment  
+      time="12pm"
+    />))
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => (
+    <Empty 
+      onAdd={action("onAdd")}
+    />))
+  .add("Show", () => 
+    <Show
+      student="Lydia Miller-Jones" 
+      interviewer={interviewer.name}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />)
+    .add("Confirm", () => 
+      <Confirm 
+        message="Delete the appointment?"
+        onConfirm={action("onConfirm")}
+        onCancel={action("onCancel")}
+      />)
+    .add("Status", () => 
+      <Status   
+        message="Deleting"
+      />)
+    .add("Error", () => 
+      <Error
+        message="Could not delete appointment."
+        onClose={action("onClose")}
+      />)
+    .add("Edit", () => 
+      <Form 
+        student="Elia Chow"
+        interviewer={3}
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+      />)
+    .add("Create", () => 
+      <Form 
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+      />)
+  
